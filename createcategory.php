b6608b135edb8fc759d6959f031d5ae17cfd3516
Palm <gentleman.palm@gmail.com>
2016-11-16 22:10:46 +0100

    Skapa kategorier
    Nu kan man skapa kategorier. Måste nu integreras i sidan för att skapa
    produkter.

---
+++ createcategory.php
@@ -0,0 +1,35 @@
+<?php
+	require_once 'functions.php';
+	require 'template/header.php';
+	require 'template/footer.php';
+	$email = $orgnr = $password = $name = $lastname = $securityNumber = $phonenumber = $town = $zipcode = $address1 = $address2 = $addressco = "";
+	//Undviker SQL-injection
+	if (isset($_POST["submit"])){
+		global $connection;
+		$title = sanitizeString($_POST["title"]);
+		$img = $connection->real_escape_string($_POST["img"]);
+
+		//Kontrollerar om alla fällt är ifyllda.
+		if ($title == "" || $img == ""){
+				echo "Not all fields were entered correctly.";
+		}else{
+			$query = querySQL("INSERT INTO Categories(title, img_path) VALUES ('$title', '$img')");
+		}
+	}
+	generateHeader("Skapa kategori");
+?>
+	<h1>Registrering</h1>
+		<form action = "" method = "POST">
+			Kategorins namn:
+			<input type = "text" name = "title"/><br><br>
+			
+			URL till standardbild:
+			<input type = "text" name = "img"/><br><br>
+
+
+			<input type = "submit" name = "submit" value = "Lägg till kategori!"/>
+			<hr>			
+		</form>
+<?php
+generateFooter();
+?>
