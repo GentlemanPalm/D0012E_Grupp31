62868ab312062800347d8a4427ff17b0944e926e
Palm <gentleman.palm@gmail.com>
2016-12-05 22:54:54 +0100

    Placeholder stuff
    Nu finns det placeholders för mycket av arbetet. Inväntar ändring av
    schemat

--- userpane.php
+++ userpane.php
@@ -18,11 +18,15 @@ if (isset($_SESSION["user_ID"])){
     $address1 = $res["address1"];
     $name = $res["first_name"];
     $lastname = $res["last_name"];
-    $town = $res["town"];
+    $town = $res["city"];
 }
 if(!isset($_SESSION["user_ID"])) {
     header("Location: index.php");
     die();
+}
+
+if (isset($_POST["submit"])) {
+    // TODO: Ta hand om informationen.
 }
 
 generateHeader("Inställningar för $name $lastname");
@@ -37,60 +41,15 @@ generateHeader("Inställningar för $name $lastname");
         return true;
     }
 </script>
-<h1>Registrering</h1>
+<h1>Uppgifter</h1>
+<a href="browseorders.php">Beställningar</a>
 <form action = "" method = "POST">
-    Produktnamn:
-    <input type = "text" name = "pname" value="<?=$res["name"]?>"/><br><br>
-
-    Ursprungligt lagersaldo:
-    <input type = "text" name = "quantity" onkeypress="return isNumber(event)" maxlength = "4" size = "4" value="<?=$res["quantity"]?>"/><br><br>
-
-    Produktbeskrivning (tillåter HTML):
-    <textarea name="desc" cols="40" rows="10"><?=$res["description"]?></textarea><br /><br />
-
-    Grundpris exklusive moms:
-    <input type = "text" name = "price" onkeypress="return isNumber(event)" maxlength = "6" size = "6" value="<?=$res["price"]?>"/><br><br>
-
-    Nuvarande exklusive moms:
-    <input type = "text" name = "cprice" onkeypress="return isNumber(event)" maxlength = "6" size = "6" value="<?=$res["current_price"]?>"/><br><br>
-    <input type = "hidden" name = "id" value="<?=$id?>" />
-    Momssats:
-    <select name="vat">
-        <option value = "<?=$res["vat"]?>">-- Samma som förut --</option>
-        <option value = "0.06">6% - Kultur, böcker, mm.</option>
-        <option value = "0.12">12% - Livsmedel, mm.</option>
-        <option value = "0.25">25% - ALLA digitala tjänster och övriga artiklar</option>
-    </select><br><br>
-
-    Kategori:
-    <select name = "cat">
-        <option value = "<?=$res["category_ID"]?>">-- Samma som förut --</option>
-        <?php
-        $result1 = querySQL("SELECT ID, title FROM Categories");
-        while ($res1 = $result1->fetch_assoc()) { ?>
-            <option value="<?=$res1["ID"]?>"><?=$res1["title"]?></option>
-            <?php
-        }
-        ?>
-    </select><br><br>
-    <?php
-        $r = querySQL("SELECT ID, path FROM Images WHERE product_ID = $id");
-        $iurl = "";
-        $imgid = "";
-        if ($r->num_rows > 0) {
-            $assoc = $r->fetch_assoc();
-            $iurl = $assoc["path"];
-            $imgid = $assoc["ID"];
-
-        }
-    ?>
-    Bild-URL. Om du vill använda kategorins standardbild, lämna detta fält blankt:
-    <input type = "text" name = "img" value="<?=$iurl?>"/><br/><br/>
-    <input type = "hidden" name = "imgid" value="<?=$imgid?>"/>
-
-    
-    <button type="submit" name = "submit" class = "btn btn-default"><span class="glyphicon glyphicon-pencil"></span> Redigera vara!</button><a href="deleteproduct.php?id=<?=$id?>"><button type="button" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span> Ta bort</button></a>
-    <hr>
+    Email: <input type = "text" name = "email" value="<?=$email?>"/><br><br>
+    Adress: <input type = "text" name = "address1" value="<?=$address1?>"/><br><br>
+    Telefon: <input type = "text" name = "phone" value="<?=$phonenumber?>"/><br><br>
+    Stad: <input type = "text" name = "city" value="<?=$town?>"/><br><br>
+    Postkod: <input type = "text" name = "zip" value="<?=$zip?>"/><br><br>
+    <button type="submit" name = "submit" class = "btn btn-default"><span class="glyphicon glyphicon-pencil"></span> Uppdatera</button>
 </form>
 <?php
 generateFooter();
